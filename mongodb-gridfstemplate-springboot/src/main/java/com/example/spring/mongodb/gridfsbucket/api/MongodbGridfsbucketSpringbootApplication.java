89a4851487e62622c5e4acd9ae15f8a6ea2fff89
kmjenniferng <ka.man.jennifer.ng@gmail.com>
2021-11-26 19:44:57 -0500

    Updated file

--- mongodb-gridfstemplate-springboot/src/main/java/com/example/spring/mongodb/gridfsbucket/api/MongodbGridfsbucketSpringbootApplication.java
+++ mongodb-gridfstemplate-springboot/src/main/java/com/example/spring/mongodb/gridfsbucket/api/MongodbGridfsbucketSpringbootApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MongodbGridfsbucketSpringbootApplication {
 
-	//https://blog.csdn.net/ai_0922/article/details/105192399
-	//https://programmer.help/blogs/springboot-learning-notes-11-using-mongodb-to-store-files.html
-	//http://mongodb.github.io/mongo-java-driver/3.7/driver/tutorials/gridfs/#download-from-gridfs
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbGridfsbucketSpringbootApplication.class, args);
 	}
-
 }
